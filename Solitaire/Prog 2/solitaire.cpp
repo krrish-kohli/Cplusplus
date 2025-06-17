833655086d89e219e10483730a7257c1aeaf078e
krrish-kohli <krrishkohli@icloud.com>
2025-06-16 17:06:30 -0700

    files updated

--- Solitaire/Prog 2/solitaire.cpp
+++ Solitaire/Prog 2/solitaire.cpp
@@ -1,11 +1,3 @@
-// Name: Krrish Kohli
-// Class (CECS 325-01)
-// Project Name (Prog 2 – Fibonacci Solitaire)
-// Due Date (06/14/2005)
-//
-// I certify that this program is my own original work. I did not copy any part of this program from
-// any other source. I further certify that I typed each and every line of code in this program.
-
 #include <iostream>
 #include <string>
 #include <vector>
